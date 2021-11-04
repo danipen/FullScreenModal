239de1c01f9c2c9d43ecd58a1d1e8ce7f9141749
Daniel <dpenalba@codicesoftware.com>
2021-11-04 14:54:17 +0100

    Add a case to reproduce the doubletapped issue

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -3,6 +3,8 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+using Avalonia.Threading;
 
 namespace FullScreenModal
 {
@@ -27,10 +29,30 @@ namespace FullScreenModal
             showNoModalButton.Click += ShowNoModalButton_Click;
             showNoModalButton.Content = "Show no modal window";
 
+            Border launcherPanel = new Border();
+            launcherPanel.Width = 200;
+            launcherPanel.Height = 100;
+            launcherPanel.Background = Brushes.LightGray;
+            launcherPanel.DoubleTapped += launcherPanel_DoubleTapped;
+            TextBlock textBlock = new TextBlock();
+            textBlock.VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center;
+            textBlock.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center;
+            textBlock.Text = "Double click me";
+            launcherPanel.Child = textBlock;
+
             panel.Children.Add(showModalButton);
             panel.Children.Add(showNoModalButton);
+            panel.Children.Add(launcherPanel);
 
             Content = panel;
+        }
+
+        void launcherPanel_DoubleTapped(object? sender, RoutedEventArgs e)
+        {
+            Window newWindow = new Window();
+            newWindow.Width = 400;
+            newWindow.Height = 400;
+            newWindow.Show();
         }
 
         private void ShowNoModalButton_Click(object? sender, RoutedEventArgs e)
